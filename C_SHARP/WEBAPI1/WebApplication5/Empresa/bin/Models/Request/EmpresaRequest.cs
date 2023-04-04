dfddc6cba314bf8b62de0f044b350609dd59d6a0
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2023-04-04 02:48:19 -0500

    Add files via upload

---
+++ C_SHARP/WEBAPI1/WebApplication5/Empresa/bin/Models/Request/EmpresaRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresa.Models.Request
+{
+    public class EmpresaRequest
+    {
+        public string emp_tipid { get; set; }
+        public string emp_numid { get; set; }
+        public string emp_nom { get; set; }
+        public string emp_dir { get; set; }
+        public string emp_ciu { get; set; }
+        public string emp_depto { get; set; }
+        public string emp_tel { get; set; }
+    }
+}
