cb411f30d4227f0278956b8a5b500b9260bd9904
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2023-04-28 10:57:07 -0500

    Added MVCDesktop

---
+++ C_SHARP/MVCDesktop1/MVCDesktop/Controladores/MainController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controladores
+{
+    public class MainController
+    {
+            public IEnumerable<Modelos.UserViewModel> GetList()
+            {
+                using (Modelos.EF.pruebaEntities db = new Modelos.EF.pruebaEntities())
+                {
+                    IEnumerable<Modelos.UserViewModel> lst = (from d in db.user
+                                                                select new Modelos.UserViewModel
+                                                                {
+                                                                    Id = d.id,
+                                                                    Email = d.email
+                                                                }).ToList();
+                return lst;
+                }
+            }
+    }
+}
