99cc820aa3791f8d3a720c56ed5db1c734572324
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2023-04-29 19:49:58 -0500

    Add EntityFrameworkExample

---
+++ C_SHARP/EntityFrameworkExample1/EjemploEntityFramework/EjemploEntityFramework/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploEntityFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(SampleEntityFramework db = new SampleEntityFramework())
+            {
+                var lst = db.gente;
+
+                /*
+                // Insertar registro
+                gente oGente = new gente();
+                oGente.nombre = "Reinaldo Urquijo";
+                oGente.edad = 38;
+                oGente.idSexo = 1;
+                db.gente.Add(oGente);
+                db.SaveChanges();
+                */
+
+                /*
+                //Actualizar registro
+                gente oGente = db.gente.Where(d => d.nombre == "ana gomez").First();
+                oGente.edad = 30;
+                db.Entry(oGente).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                */
+
+                /*
+                //Eliminar registro (si el find no devuelve nulo)
+                gente oGente = db.gente.Find(3);
+                db.gente.Remove(oGente);
+                db.SaveChanges();
+                */
+
+                foreach (var oGente_ in lst)
+                {
+                    Console.WriteLine(oGente_.nombre);
+                }
+
+
+            }
+
+
+            Console.Read();
+        }
+    }
+}
