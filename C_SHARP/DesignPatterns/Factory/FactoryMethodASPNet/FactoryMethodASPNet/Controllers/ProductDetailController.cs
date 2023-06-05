876012407de1a72182da54ecf97ed40737b49b8a
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2023-06-05 16:30:48 -0500

    Factory Design Pattern

---
+++ C_SHARP/DesignPatterns/Factory/FactoryMethodASPNet/FactoryMethodASPNet/Controllers/ProductDetailController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Schema;
+using Tools.Earn;
+
+namespace FactoryMethodASPNet.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        public IActionResult Index(decimal total)
+        {
+            //factories
+            LocalEarnFactory localEarnFactory = new LocalEarnFactory(0.20m);
+            ForeignEarnFactory foreignEarnFactory = new ForeignEarnFactory(0.30m, 20);
+
+            //products
+            var localEarn = localEarnFactory.GetEarn();
+            var foreignEarn = foreignEarnFactory.GetEarn();
+
+            // total
+            ViewBag.totalLocal = total + localEarn.Earn(total);
+            ViewBag.totalForeign = total + foreignEarn.Earn(total);
+
+            return View();
+        }
+    }
+}
