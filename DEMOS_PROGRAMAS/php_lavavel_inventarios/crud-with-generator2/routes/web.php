c238ae8834730106b4c07037b4c01af57fe8aa7b
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2025-03-08 23:37:49 -0500

    Ajustes

---
+++ DEMOS_PROGRAMAS/php_lavavel_inventarios/crud-with-generator2/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('inventarios', App\Http\Controllers\InventarioController::class)->middleware('auth');
